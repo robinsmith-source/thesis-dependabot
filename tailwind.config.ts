e112db7eb5fbb86d0ce337ab0ce07a6b32b1811a
Robin Schmidt <schmidtrobin02@gmail.com>
2023-11-06 18:09:22 +0100

    feat: install next-ui along installation guide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,17 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import {type Config} from "tailwindcss";
+import {fontFamily} from "tailwindcss/defaultTheme";
+import {nextui} from "@nextui-org/react";
 
 export default {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
+    content: ["./src/**/*.tsx",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["var(--font-sans)", ...fontFamily.sans],
+            },
+        },
+        darkMode: "class",
     },
-  },
-  plugins: [],
+    plugins: [nextui()],
 } satisfies Config;
