2332744d7d4b33abba12f36e0232c68f9cc22dce
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2023-12-18 05:31:29 +0100

    fix: don't even know but it didn't work before, now it does

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1734,14 +1734,14 @@ async function seedUserRecipes() {
       },
     },
   }));
-
-  main()
-    .then(async () => {
-      await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-      console.error(e);
-      await prisma.$disconnect();
-      process.exit(1);
-    });
 }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
