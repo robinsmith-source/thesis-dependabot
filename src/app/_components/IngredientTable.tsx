8d09d0e23dce49428c77c4d6c128f5cbc53831c8
sabrinaturni <sabrina.turni@gmx.de>
2024-01-11 00:24:31 +0100

    feat: counter input for decrement and increment portion size

--- src/app/_components/IngredientTable.tsx
+++ src/app/_components/IngredientTable.tsx
@@ -1,6 +1,6 @@
 "use client";
+import type { Selection } from "@nextui-org/react";
 import {
-  Input,
   Table,
   TableBody,
   TableCell,
@@ -8,7 +8,6 @@ import {
   TableHeader,
   TableRow,
 } from "@nextui-org/react";
-import type { Selection } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { convertUnitName } from "~/app/utils";
 import {
@@ -73,8 +72,91 @@ export default function IngredientTable({
     setShouldEmitSelection(true);
   }
 
+  function handlePortionSize(count: number) {
+    if (portionSize + count > 0) {
+      setPortionSize(portionSize + count);
+    }
+  }
+
   return (
     <>
+      {isPortionable && (
+        <>
+          <label
+            htmlFor="quantity-input"
+            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Choose portion size:
+          </label>
+          <div className="relative flex max-w-[8rem] items-center">
+            <button
+              type="button"
+              id="decrement-button"
+              onClick={() => {
+                handlePortionSize(-1);
+              }}
+              data-input-counter-decrement="quantity-input"
+              className="h-11 rounded-s-lg border border-gray-300 p-3 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-800 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+            >
+              <svg
+                className="h-3 w-3 text-gray-900 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 2"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h16"
+                />
+              </svg>
+            </button>
+            <input
+              onChange={({ target }) => {
+                const parsedValue = parseInt(target.value);
+                if (!isNaN(parsedValue) && parsedValue > 0) {
+                  setPortionSize(parsedValue);
+                }
+              }}
+              type="text"
+              id="quantity-input"
+              value={portionSize + ""}
+              data-input-counter
+              disabled
+              aria-describedby="helper-text-explanation"
+              className="block h-11 w-full border-x-0 border-gray-300 py-2.5 text-center text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-800 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            />
+            <button
+              type="button"
+              id="increment-button"
+              onClick={() => {
+                handlePortionSize(1);
+              }}
+              data-input-counter-increment="quantity-input"
+              className="h-11 rounded-e-lg border border-gray-300 p-3 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-800 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+            >
+              <svg
+                className="h-3 w-3 text-gray-900 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 18"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 1v16M1 9h16"
+                />
+              </svg>
+            </button>
+          </div>
+        </>
+      )}
       <Table
         aria-label="Ingredient Table"
         className={`max-w-xs ${className}`}
@@ -102,19 +184,6 @@ export default function IngredientTable({
           ))}
         </TableBody>
       </Table>
-      {isPortionable && (
-        <Input
-          onValueChange={(value) => {
-            setPortionSize(parseInt(value));
-          }}
-          size="sm"
-          type="number"
-          min={1}
-          defaultValue={portionSize + ""}
-          placeholder="required portion"
-          className="w-40"
-        />
-      )}
     </>
   );
 }
