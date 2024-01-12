e0f170fe160f995ed4e02795a257445c5158c70d
Robin Schmidt <schmidtrobin02@gmail.com>
2024-01-12 15:48:35 +0100

    feat: add additional recipe section, add reveal animation on scroll

---
+++ src/app/lib/animations/FadeIn.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { motion, useAnimation } from "framer-motion";
+import { Children, ReactNode, useEffect, useRef } from "react";
+import { useInView } from "framer-motion";
+
+interface FadeInProps {
+  className?: string;
+  children: ReactNode | ReactNode[];
+  direction?: "top" | "bottom" | "left" | "right";
+  offset?: number;
+}
+
+export const FadeIn = ({
+  className,
+  children,
+  direction = "bottom",
+  offset = 0,
+}: FadeInProps) => {
+  const childrenArray = Children.toArray(children);
+
+  const controls = useAnimation();
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const variants = {
+    hidden: { opacity: 0, y: 0, x: 0 },
+    visible: { opacity: 1, y: 0, x: 0 },
+  };
+
+  switch (direction) {
+    case "top":
+      variants.hidden.y = -40;
+      break;
+    case "bottom":
+      variants.hidden.y = 40;
+      break;
+    case "left":
+      variants.hidden.x = -40;
+      break;
+    case "right":
+      variants.hidden.x = 40;
+      break;
+  }
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start("visible");
+    }
+  }, [isInView]);
+
+  return (
+    <div>
+      {Array.isArray(childrenArray) &&
+        childrenArray.map((child, index) => {
+          return (
+            <motion.div
+              ref={ref}
+              key={index}
+              variants={variants}
+              initial="hidden"
+              animate={controls}
+              transition={{
+                type: "spring",
+                damping: 20,
+                stiffness: 100,
+                delay: 0.2 * index + offset,
+                bounce: 0.4,
+              }}
+              whileInView="animate"
+              className={className}
+            >
+              {child}
+            </motion.div>
+          );
+        })}
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,70 @@
 import { api } from "~/trpc/server";
-import { Divider, Image } from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
 import NextImage from "next/image";
 import RecipeCardsSection from "~/app/_components/RecipeCardsSection";
+import { FadeIn } from "~/app/lib/animations/FadeIn";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
+  const featuredRecipes = await api.recipe.getRecipeCards.query({
+    take: 4,
+    //Sort by total rating or something
+  });
   const latestRecipes = await api.recipe.getRecipeCards.query({
-    take: 20,
+    take: 4,
+    orderBy: "NEWEST",
   });
 
   return (
     <main className="flex flex-col items-center">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <div className="ml-5">
-          <h1 className="text-7xl font-bold">Welcome to Goose Chef!</h1>
-          <p className="mt-8 text-4xl font-semibold">
-            The cooking website with your favourite Recipes!
-          </p>
-          <p className="mt-8 text-2xl">
-            Add your own Recipes, share them with Friends and Family. Or explore
-            new Recipes from other Cooking-Enthusiasts!
-          </p>
+      <FadeIn>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div className="ml-5">
+            <h1 className="text-7xl font-bold">Welcome to Goose Chef!</h1>
+            <p className="mt-8 text-4xl font-semibold">
+              The cooking website with your favourite Recipes!
+            </p>
+            <p className="mt-8 text-2xl">
+              Add your own Recipes, share them with Friends and Family. Or
+              explore new Recipes from other Cooking-Enthusiasts!
+            </p>
+          </div>
+          <div className="mb-4 flex justify-center">
+            {/* Goose chef logo */}
+            <Image
+              as={NextImage}
+              width={500}
+              height={500}
+              src="/images/goose_chef_paperbag.png"
+              alt="Logo"
+              className="h-120 w-120 mb-2 object-contain"
+            />
+          </div>
         </div>
-        <div className="mb-4 flex justify-center">
-          {/* Goose chef logo */}
-          <Image
-            as={NextImage}
-            width={500}
-            height={500}
-            src="/images/goose_chef_paperbag.png"
-            alt="Logo"
-            className="h-120 w-120 mb-2 object-contain"
-          />
-        </div>
-      </div>
-      <Divider className="my-4" />
-      <RecipeCardsSection recipes={latestRecipes} />
+      </FadeIn>
+
+      <section className="flex h-screen flex-col items-start justify-center gap-32">
+        <FadeIn direction="left">
+          <div className="mb-4 text-start">
+            <h2 className="text-4xl font-semibold">Featured Recipes</h2>
+            <h2 className="text-2xl font-light text-foreground-600">
+              Here are some of our favourite Recipes!
+            </h2>
+          </div>
+          <RecipeCardsSection recipes={featuredRecipes} layout="flex" />
+        </FadeIn>
+
+        <FadeIn direction="left">
+          <div className="mb-4 text-start">
+            <h2 className="text-4xl font-semibold"> Latest Recipes </h2>
+            <h2 className="text-2xl font-light text-foreground-600">
+              Here are some of our latest Recipes!
+            </h2>
+          </div>
+          <RecipeCardsSection recipes={latestRecipes} layout="flex" />
+        </FadeIn>
+      </section>
     </main>
   );
 }
