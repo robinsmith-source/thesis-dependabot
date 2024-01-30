9a8cb4af6ff2e387e9e98d4a1ba75e9f715263f6
Robin Schmidt <schmidtrobin02@gmail.com>
2024-01-30 22:37:13 +0100

    fix: make tests independent

--- e2e/global.teardown.ts
+++ e2e/global.teardown.ts
@@ -2,16 +2,23 @@ import { PrismaClient } from "@prisma/client";
 
 export default async function cleanDb() {
   const prisma = new PrismaClient();
-  await prisma.recipe
-    .deleteMany({
-      where: {
-        author: {
-          //TODO: figure out a better way to do this
-          name: {
-            startsWith: "Testing",
-          },
+  await prisma.recipe.deleteMany({
+    where: {
+      author: {
+        //TODO: figure out a better way to do this
+        name: {
+          startsWith: "Testing",
         },
       },
-    })
-    .finally(() => prisma.$disconnect());
+    },
+  });
+  await prisma.shoppingList.deleteMany({
+    where: {
+      author: {
+        name: {
+          startsWith: "Testing",
+        },
+      },
+    },
+  });
 }
--- e2e/shoppingList-create.spec.ts
+++ e2e/shoppingList-create.spec.ts
@@ -16,45 +16,65 @@ test("Should create a empty shopping list and make sure it gets created", async 
   await page.getByPlaceholder("This is my shopping list").click();
   await page.getByPlaceholder("This is my shopping list").fill("Description");
   await page.getByRole("button", { name: "Submit" }).click();
-  await page
-    .getByRole("heading", { name: "ShoppingList1" })
-    .first()
-    .isVisible();
+  await expect(
+    page.getByRole("heading", { name: "ShoppingList1" }).first(),
+  ).toBeVisible();
   await expect(page.locator(".text-left").first()).toContainText("Description");
   await expect(page.locator(".relative > p").first()).toContainText(
     "No items in this shopping list",
   );
 });
-test("Should create a shopping list with all recipe ingredients and make sure it gets created", async ({
+
+test("Should create a shopping list from recipe page and add all ingredients", async ({
   page,
 }) => {
-  await page.goto("http://localhost:3000/");
-  await page.getByRole("button", { name: "Explore Recipes" }).click();
-  await page.getByRole("button", { name: "Spaghetti Aglio e Olio" }).click();
+  await page.goto("http://localhost:3000/recipe/create");
+  await page.getByPlaceholder("My tasty Pizza").click();
+  await page
+    .getByPlaceholder("My tasty Pizza")
+    .fill("Testing Recipe for Shopping List");
+  await page.getByLabel("Easy,").click();
+  await page.getByLabel("Medium", { exact: true }).getByText("Medium").click();
+  await page.getByPlaceholder("My grandma used to make this").click();
+  await page
+    .getByPlaceholder("My grandma used to make this")
+    .fill("Description");
+  await page.getByLabel("Recipe Tags (0/10)").click();
+  await page.getByLabel("Recipe Tags (0/10)").fill("tag");
+  await page.getByLabel("Recipe Tags (0/10)").press("Enter");
+  await page.getByRole("button", { name: "Add Step" }).click();
+  await page.getByLabel("Step Description").fill("Step1");
+  await page
+    .getByRole("button", { name: "Add Ingredient", exact: true })
+    .click();
+  await page.getByLabel("Name", { exact: true }).fill("Ingredient1");
+  await page.getByRole("button", { name: "Submit" }).click();
+
   await page.waitForURL(/\/recipe\/c[a-z0-9]{24}/);
+
   await page
     .locator("div")
     .filter({ hasText: /^Shopping ListSelect ingredients$/ })
-    .getByRole("button")
+    .locator("button")
     .nth(2)
     .click();
   await page.getByPlaceholder("My shopping list", { exact: true }).click();
   await page
     .getByPlaceholder("My shopping list", { exact: true })
-    .fill("ShoppingList1");
+    .fill("ShoppingList2");
   await page.getByPlaceholder("This is my shopping list").click();
   await page.getByPlaceholder("This is my shopping list").fill("Description");
   await page.getByRole("button", { name: "Submit" }).click();
-
   await page.getByPlaceholder("Choose a shopping list").click();
   await page.getByPlaceholder("Choose a shopping list").fill("ShoppingList2");
+  await page.getByText("ShoppingList2").click();
+
+  await page.getByLabel("Select All").locator("svg").click();
   await page
-    .getByRole("option", { name: "ShoppingList1", exact: true })
-    .first()
-    .click();
-  await page.getByLabel("Select All").click();
-  await page
-    .getByRole("button", { name: "Add Ingredients to shopping" })
+    .getByRole("button", {
+      name: "Add Ingredient to shopping list",
+      exact: true,
+    })
     .click();
 
   await page.waitForURL(/\/shopping-list/);
@@ -62,7 +82,7 @@ test("Should create a shopping list with all recipe ingredients and make sure it
     page.getByRole("heading", { name: "ShoppingList2" }).first().isVisible(),
   );
   await expect(page.locator(".text-left").first()).toContainText("Description");
-  await expect(page.locator(".relative > p").first()).toContainText(
-    "Add ItemsRemove ItemsAmountIngredient2 tspSalt300 gSpaghetti4 tbspOlive Oil4 Garlic cloves1 tspRed Pepper Flakes1 tbspParsley1 tspPepper",
+  await expect(page.getByLabel("Ingredient Table")).toContainText(
+    "AmountIngredient1 gIngredient1",
   );
 });
