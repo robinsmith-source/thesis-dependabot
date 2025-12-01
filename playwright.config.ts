a57e24ad6a2b66bf8e9e2202c0c90fd55f96ec04
Robin Schmidt <schmidtrobin02@gmail.com>
2025-12-01 11:38:02 +0100

    chore(ci): remove sensitive data, simplify tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,31 +34,7 @@ export default defineConfig({
     {
       name: "Chromium",
       use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
-      name: "Firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "Webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-
-    //Test against mobile viewports.
-    {
-      name: "Mobile Chrome",
-      use: { ...devices["Pixel 7"] },
-    },
-    {
-      name: "Mobile Firefox",
-      use: { ...devices["Pixel 7"] },
-    },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
-    },
+    }
   ],
 
   /* Run your local dev server before starting the tests */
